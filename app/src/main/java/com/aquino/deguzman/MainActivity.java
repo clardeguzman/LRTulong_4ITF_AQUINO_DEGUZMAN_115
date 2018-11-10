71df125c0808cc1511949185a22af8abd647cd1f
JunickMarco <2015081561@ust-ics.mygbiz.com>
2018-11-10 09:52:58 +0800

    Home to Pricecheck Page Transition

--- app/src/main/java/com/aquino/deguzman/MainActivity.java
+++ app/src/main/java/com/aquino/deguzman/MainActivity.java
@@ -1,13 +1,30 @@
 package com.aquino.deguzman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (LinearLayout) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Pricecheck.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/aquino/deguzman/Pricecheck.java
@@ -0,0 +1,13 @@
+package com.aquino.deguzman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pricecheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pricecheck);
+    }
+}
