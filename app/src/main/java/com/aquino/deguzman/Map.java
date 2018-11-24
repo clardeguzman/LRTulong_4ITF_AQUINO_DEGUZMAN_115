4cd956c4f1b62e32ff12eb10c8c5dad7095f62b9
JunickMarco <2015081561@ust-ics.mygbiz.com>
2018-11-24 21:03:26 +0800

    Added Map Pinch Functionality

--- app/src/main/java/com/aquino/deguzman/Map.java
+++ app/src/main/java/com/aquino/deguzman/Map.java
@@ -3,11 +3,16 @@ package com.aquino.deguzman;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.chrisbanes.photoview.PhotoView;
+
 public class Map extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
+        photoView.setImageResource(R.drawable.mapmanila);
     }
 }
